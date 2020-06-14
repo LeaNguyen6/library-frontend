fe923e5e3a9cb6f2752bcbad91e4a63c7914e0cd
Invisible16 <julia.nguyen0616@gmail.com>
2020-06-14 22:57:12 +0700

    NavBar & Router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+//import CssBaseline from '@material-ui/core/CssBaseline';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <NavBar>
+
+          {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/transactions">
+              <About />
+            </Route>
+            <Route path="/books">
+              <About />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route exact path="/">
+              <div>Home</div>
+            </Route>
+          </Switch>
+        </NavBar>
+      </div>
+    </Router>
   );
 }
+function Home() {
+  return <h2>Home</h2>;
+}
 
+function About() {
+  return <h2>Transaction</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
 export default App;
